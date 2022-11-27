d45f4be52b41660a3e8904d140a9513e98b25432
Samiksha Paudel <samikshapaudel22@gmail.com>
2022-11-27 12:50:02 +0545

    added adapter

--- app/src/main/java/com/example/becomputer/ProductActivity.java
+++ app/src/main/java/com/example/becomputer/ProductActivity.java
@@ -1,11 +1,15 @@
 package com.example.becomputer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.becomputer.adapter.ProductAdapter;
 import com.example.becomputer.model.Product;
+
+import java.util.ArrayList;
 
 public class ProductActivity extends AppCompatActivity {
     private RecyclerView rvProduct;
@@ -49,6 +53,19 @@ public class ProductActivity extends AppCompatActivity {
                     product3.setName("Dell 15 inch");
                     product3.setPrice(1800.00);
                     product3.setCategory("Laptop");
+
+                    ArrayList<Product> productArrayList = new ArrayList<>();
+                    productArrayList.add(product);
+                    productArrayList.add(product1);
+                    productArrayList.add(product2);
+                    productArrayList.add(product3);
+
+
+
+                    ProductAdapter adapter= new ProductAdapter(ProductActivity.this , productArrayList);
+                    rvProduct.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL , false));
+                    rvProduct.setAdapter(adapter);
+
 
                 }
 
