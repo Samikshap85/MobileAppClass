8da3928f1d1ada71a37953f8e4764a9db89747cc
Samiksha Paudel <samikshapaudel22@gmail.com>
2022-12-05 13:35:31 +0545

    database adapter added

--- app/src/main/java/com/example/becomputer/DatabaseActivity.java
+++ app/src/main/java/com/example/becomputer/DatabaseActivity.java
@@ -1,36 +1,77 @@
 package com.example.becomputer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.becomputer.app.App;
 import com.example.becomputer.database.AppDatabase;
 import com.example.becomputer.database.dao.FoodDao;
 import com.example.becomputer.database.model.FoodEntity;
+import com.example.becomputer.databinding.ActivityDatabaseBinding;
 
 public class DatabaseActivity extends AppCompatActivity {
-    private AppDatabase db;
-    private FoodDao foodDao;
+    private ActivityDatabaseBinding binding;
 
+    ////    private FoodDao foodDao;
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_database);
+        binding = ActivityDatabaseBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot(); //find view by id garnu pardaina binding garyo vani
+        setContentView(view);
 
-        db= App.db;
-        foodDao = db.foodDao();
+        binding.btnAddRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DatabaseActivity.this, "Button Clicked", Toast.LENGTH_LONG).show(); //call back fxn ho so this vanni kam gardaina
+            }
 
-        FoodEntity food1= new FoodEntity();{
-            food1.setName("Burger");
-            food1.setFood_id(23);
-            food1.setPrice("250");
-            food1.setStatus("available");
 
-           // foodDao.insertItem(food1); //database ma entry gareko
-            foodDao.deleteItem(food1);
-            Log.d("TEST_FOOD" , "DB Record Size" + foodDao.getAllItems().size());
-        }
+        });
+        binding.btnRemoveRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(DatabaseActivity.this, "Button Clicked", Toast.LENGTH_LONG).show(); //call back fxn ho so this vanni kam gardaina
+            }
+        });
+
+        binding.rvRecords.setLayoutManager(new LinearLayoutManager( //seting recycler view layout or desgin
+                this,
+                LinearLayoutManager.VERTICAL,
+                false
+        ));
     }
 }
+
+
+
+
+
+
+
+
+
+
+//
+//        db= App.db;
+//        foodDao = db.foodDao();
+//
+//        FoodEntity food1= new FoodEntity();{
+//            food1.setName("Burger");
+//            food1.setFood_id(23);
+//            food1.setPrice("250");
+//            food1.setStatus("available");
+//
+//           // foodDao.insertItem(food1); //database ma entry gareko
+//            foodDao.deleteItem(food1);
+//            Log.d("TEST_FOOD" , "DB Record Size" + foodDao.getAllItems().size());
+//        }
+//    }
+//}
