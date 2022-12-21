676834e8e84f025d94eed43cb298b61de8af7126
Samiksha Paudel <samikshapaudel22@gmail.com>
2022-12-21 13:38:46 +0545

    firebase

--- app/src/main/java/com/example/becomputer/LoginActivity.java
+++ app/src/main/java/com/example/becomputer/LoginActivity.java
@@ -1,19 +1,30 @@
 package com.example.becomputer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class LoginActivity extends AppCompatActivity {
+    String TAG = "Firebase";
     Button btLogin , btSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initFirebase();
+
         btLogin= findViewById(R.id.btLogin);
 
         btLogin.setOnClickListener(new View.OnClickListener() {
@@ -43,4 +54,26 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initFirebase() {
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        String msg = String.format(R.string.msg_token_fmt +":"+ token);
+                        Log.d(TAG, msg);
+                        Toast.makeText(LoginActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
+}
